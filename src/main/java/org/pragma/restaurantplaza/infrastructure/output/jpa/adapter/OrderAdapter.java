774ad354a540736f55cb80988e1b37743f16849c
Juan Pablo Avendaño <jpavendanopi@cesde.net>
2023-08-29 15:42:37 -0500

    historia14: Enviar mensajes de pedidos pendientes

--- src/main/java/org/pragma/restaurantplaza/infrastructure/output/jpa/adapter/OrderAdapter.java
+++ src/main/java/org/pragma/restaurantplaza/infrastructure/output/jpa/adapter/OrderAdapter.java
@@ -1,5 +1,8 @@
 package org.pragma.restaurantplaza.infrastructure.output.jpa.adapter;
 
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
 import lombok.RequiredArgsConstructor;
 import org.pragma.restaurantplaza.application.dto.OrderRequest;
 import org.pragma.restaurantplaza.domain.model.Meal;
@@ -21,6 +24,10 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class OrderAdapter implements IOrderPersistencePort {
+
+    private static final String ACCOUNT_SID = "YOUR_TWILIO_ACCOUNT_SID";
+    private static final String AUTH_TOKEN = "YOUR_TWILIO_AUTH_TOKEN";
+    private static final String TWILIO_PHONE_NUMBER = "YOUR_TWILIO_PHONE_NUMBER";
     private final IOrderRepository orderRepository;
     private final OrderEntityMapper orderEntityMapper;
     private final IUserRepository userRepository;
@@ -29,15 +36,18 @@ public class OrderAdapter implements IOrderPersistencePort {
 
     @Override
     public void createOrder(OrderRequest orderRequest) {
-        UserEntity client = userRepository.findById(orderRequest.getId())
+        UserEntity user = userRepository.findById(orderRequest.getId())
                 .orElseThrow(() -> new EntityNotFoundException("Client not found"));
 
         List<MealEntity> selectedMealEntities = selectedMeals.stream()
                 .map(mealEntityMapper::toMealEntity)
                 .collect(Collectors.toList());
-        Order order = getOrder(orderRequest, client, selectedMealEntities);
+        Order order = getOrder(orderRequest, user, selectedMealEntities);
 
         orderRepository.save(orderEntityMapper.toOrderEntity(order));
+
+        sendOrderNotification(user, "1234", orderRequest);
+
     }
 
     private static Order getOrder(OrderRequest orderRequest, UserEntity client, List<MealEntity> selectedMealEntities) {
@@ -50,5 +60,36 @@ public class OrderAdapter implements IOrderPersistencePort {
         order.setQuantity(orderRequest.getQuantity());
         order.setOrderStatus(OrderStatus.PENDING);
         return order;
+    }
+
+
+
+    public void sendOrderNotification(UserEntity user, String pin,OrderRequest orderRequest){
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+        String messageBody = "";
+        if(orderRequest.getOrderStatus().equals(OrderStatus.PENDING))
+            messageBody = "Tu pedido está en pendientes. ";
+
+        if(orderRequest.getOrderStatus().equals(OrderStatus.IN_PREPARATION))
+            messageBody = "Tu pedido está en preparacion para recoger. ";
+
+        if(orderRequest.getOrderStatus().equals(OrderStatus.READY))
+            messageBody = "Tu pedido está listo para recoger. ";
+
+        if(orderRequest.getOrderStatus().equals(OrderStatus.DELIVERED))
+            messageBody = "Tu pedido fue entregado. ";
+
+        if(orderRequest.getOrderStatus().equals(OrderStatus.CANCELED))
+            messageBody = "Tu pedido fue cancelado. ";
+
+
+        Message message = Message.creator(
+                new PhoneNumber(user.getPhone()),
+                new PhoneNumber(TWILIO_PHONE_NUMBER),
+                messageBody+"Presenta el siguiente pin de seguridad: "+pin
+        ).create();
+
+       System.out.println("Mensaje SID: " + message.getSid());
+
     }
 }
